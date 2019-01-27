72a31267a25db1e979e9a64457698411e609c5fe
Akram Izzeldin <akram.izz.kh@gmail.com>
2019-01-27 21:10:44 +0200

    New ValueType for different objects in interpreter
    Added a new type of value to the interpreter values.
    This new type is used for large types (strings, instances, etc) that
    need to be allocated in the heap as opposed to the previously existing
    types (number, bool, nil) that are allocated in the stack.

---
+++ src/object.h
@@ -0,0 +1,35 @@
+#ifndef QED_OBJECT_H
+#define QED_OBJECT_H
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+   OBJ_STRING
+} ObjType;
+
+struct sObj {
+   ObjType type;
+};
+
+struct sObjString {
+   Obj obj;
+   int length;
+   char *chars;
+};
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+// using a function because [value] is used twice (in function) thus if 
+// we wrote it here directly it will result in evaluating the [value]
+// expression twice!
+#define IS_STRING(value) is_obj_type(value, OBJ_STRING)
+
+#define AS_STRING(value) ((ObjString *)AS_OBJ(value))
+#define AS_CSTRING(value) AS_STRING(value)->chars
+
+static inline bool is_obj_type(Value value, ObjType type) {
+   return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
